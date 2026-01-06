9b017aeb9bc42514644d424422167f58ab933c81
kaviin360-pixel <kaviin360@gmail.com>
2026-01-06 09:45:11 +0530

    commit initial files

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let characters = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+  "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+  "0","1","2","3","4","5","6","7","8","9",
+  "!","@","#","$","%","^","&","*","(",")","_","-","+","=",
+  "{","[","}","]","|","\\",":",";","\"","'","<",">","?", "/"
+];
+let pass1el=document.getElementById("pass1-el")
+let pass2el=document.getElementById("pass2-el")
+function genPass(){
+    pass1el.textContent = ""
+    pass2el.textContent = ""
+    for(let i=0;i<16;i++){
+        let passel1= Math.floor(Math.random()*characters.length)
+        pass1el.textContent += characters[passel1]
+        let passel2 = Math.floor(Math.random()*characters.length)
+        pass2el.textContent += characters[passel2]
+    }
+}
